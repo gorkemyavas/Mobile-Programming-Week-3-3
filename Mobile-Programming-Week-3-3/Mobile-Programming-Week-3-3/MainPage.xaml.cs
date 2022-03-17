162479835749587d606807c8cc11e50f13682c32
gorkem <gorkemyavas98@gmail.com>
2022-03-17 11:38:25 +0300

    Add project files.

---
+++ Mobile-Programming-Week-3-3/Mobile-Programming-Week-3-3/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Mobile_Programming_Week_3_3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var formattedString = new FormattedString();
+
+            NamedSize[] namedSizes = new NamedSize[]
+            {
+                NamedSize.Micro,
+                NamedSize.Small,
+                NamedSize.Default,
+                NamedSize.Medium,
+                NamedSize.Large
+            };
+
+            foreach (var namedSize in namedSizes)
+            {
+                var fntSize = Device.GetNamedSize(namedSize, typeof(Label));
+                formattedString.Spans.Add(
+                    new Span
+                    {
+                        Text = String.Format("{0} için Font Size Değeri = {1:F2}\n", namedSize, fntSize),
+                        FontSize = fntSize
+                    });
+                if (namedSize != namedSizes.Last())
+                {
+                    formattedString.Spans.Add(new Span
+                    {
+                        Text = Environment.NewLine + Environment.NewLine
+                    });
+                }
+            }
+
+            Content = new Label
+            {
+                FormattedText = formattedString,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            InitializeComponent();
+        }
+    }
+}
